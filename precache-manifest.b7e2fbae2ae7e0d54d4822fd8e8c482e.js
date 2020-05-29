53f9990dd130385e56d714b8024685495a70be4f
Angel Cabrera <soyangelcabrera@gmail.com>
2020-05-28 20:16:20 -0400

    Updates

---
+++ precache-manifest.b7e2fbae2ae7e0d54d4822fd8e8c482e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbade8cf0987ad80fda9094d28c68a6",
+    "url": "/react-redux-football-manager-app/index.html"
+  },
+  {
+    "revision": "eb6bb01929904ef905c5",
+    "url": "/react-redux-football-manager-app/static/css/main.cdcfab4f.chunk.css"
+  },
+  {
+    "revision": "6d3293b60aac0108d52e",
+    "url": "/react-redux-football-manager-app/static/js/2.b8613593.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-football-manager-app/static/js/2.b8613593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6bb01929904ef905c5",
+    "url": "/react-redux-football-manager-app/static/js/main.0f1768da.chunk.js"
+  },
+  {
+    "revision": "577eacb56581f7b553d9",
+    "url": "/react-redux-football-manager-app/static/js/runtime-main.9372d817.js"
+  },
+  {
+    "revision": "aa6d54bfa22c49cf58b85efd6f82d010",
+    "url": "/react-redux-football-manager-app/static/media/cancha.aa6d54bf.svg"
+  }
+]);
